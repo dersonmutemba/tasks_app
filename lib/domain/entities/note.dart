3f657c74ba5aa35350b61316c21e35ac3f403314
dersonmutemba <dersonmutemba@gmail.com>
2023-02-17 15:27:13 -0600

    overrided equals and hashCode

--- lib/domain/entities/note.dart
+++ lib/domain/entities/note.dart
@@ -1,8 +1,26 @@
-class Note{
+class Note {
   final int id;
   final String title;
   final String content;
   final DateTime createdAt;
   final DateTime lastEdited;
-  Note({required this.id, required this.title, required this.content, required this.createdAt, required this.lastEdited});
+  Note(
+      {required this.id,
+      required this.title,
+      required this.content,
+      required this.createdAt,
+      required this.lastEdited});
+
+  @override
+  bool operator ==(Object other) =>
+      other is Note &&
+      other.runtimeType == runtimeType &&
+      other.id == id &&
+      other.title == title &&
+      other.content == content &&
+      other.createdAt == createdAt &&
+      other.lastEdited == lastEdited;
+
+  @override
+  int get hashCode => ("$id$title$content$createdAt$lastEdited").hashCode;
 }
