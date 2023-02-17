48a9f90d4edbb66ad2ef8272277f1978a40b6af7
dersonmutemba <dersonmutemba@gmail.com>
2023-02-17 15:18:57 -0600

    created entity `Note`

---
+++ lib/domain/entities/note.dart
@@ -0,0 +1,8 @@
+class Note{
+  final int id;
+  final String title;
+  final String content;
+  final DateTime createdAt;
+  final DateTime lastEdited;
+  Note({required this.id, required this.title, required this.content, required this.createdAt, required this.lastEdited});
+}
