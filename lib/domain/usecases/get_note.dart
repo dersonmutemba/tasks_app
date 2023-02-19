2e6dda49c4d458c9780db83e312b68a125e8ed59
dersonmutemba <dersonmutemba@gmail.com>
2023-02-19 15:09:14 -0600

    completed writing code for `GetNote`

--- lib/domain/usecases/get_note.dart
+++ lib/domain/usecases/get_note.dart
@@ -1,6 +1,13 @@
-import 'package:tasks_app/domain/contracts/note_contract.dart';
+import 'package:dartz/dartz.dart';
+import 'package:uuid/uuid.dart';
+
+import '../../core/error/failure.dart';
+import '../contracts/note_contract.dart';
+import '../entities/note.dart';
 
 class GetNote{
   final NoteContract contract;
   GetNote(this.contract);
+
+  Future<Either<Failure, Note>> execute({required Uuid id}) => contract.getNote(id);
 }
