009ec6bea5061d4e707d42a6ca4934417e9cd1c1
dersonmutemba <dersonmutemba@gmail.com>
2023-02-22 19:31:20 +0200

    refactored `NoteModel`

--- lib/data/models/note_model.dart
+++ lib/data/models/note_model.dart
@@ -3,5 +3,16 @@ import 'package:uuid/uuid.dart';
 import '../../domain/entities/note.dart';
 
 class NoteModel extends Note {
-  NoteModel({required Uuid id, required String title, required String content, required DateTime createdAt, required DateTime lastEdited}) : super(id)
+  NoteModel(
+      {required Uuid id,
+      required String title,
+      required String content,
+      required DateTime createdAt,
+      required DateTime lastEdited})
+      : super(
+            id: id,
+            title: title,
+            content: content,
+            createdAt: createdAt,
+            lastEdited: lastEdited);
 }
