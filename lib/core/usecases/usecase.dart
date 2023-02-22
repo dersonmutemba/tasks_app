b877f2f98a94a6edd4a94bb1d5ae98ee1e465e68
dersonmutemba <dersonmutemba@gmail.com>
2023-02-22 16:19:09 +0200

    added usecase base class

---
+++ lib/core/usecases/usecase.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+
+import '../error/failure.dart';
+
+abstract class UseCase<Type, Params> {
+  Future<Either<Failure, Type>> call(Params params);
+}
+
+class NoParams {
+  @override
+  bool operator ==(Object other) =>
+      other is NoParams && other.runtimeType == runtimeType;
+}
