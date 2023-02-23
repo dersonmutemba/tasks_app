ae342d9f61b55af49af37c05729cae2c5ad65866
dersonmutemba <dersonmutemba@gmail.com>
2023-02-23 23:04:26 +0200

    added tests for remote data source

--- test/data/repositories/note_repository_test.dart
+++ test/data/repositories/note_repository_test.dart
@@ -1,10 +1,13 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:tasks_app/core/platform/network_info.dart';
 import 'package:tasks_app/data/datasources/note_local_data_source.dart';
 import 'package:tasks_app/data/datasources/note_remote_data_source.dart';
+import 'package:tasks_app/data/models/note_model.dart';
 import 'package:tasks_app/data/repositories/note_repository.dart';
+import 'package:tasks_app/domain/entities/note.dart';
 
 import 'note_repository_test.mocks.dart';
 
@@ -25,5 +28,38 @@ void main() {
         remoteDataSource: mockNoteRemoteDataSource,
         localDataSource: mockNoteLocalDataSource,
         networkInfo: mockNetworkInfo);
+  });
+
+  const String testId = "110ec58a-a0f2-4ac4-8393-c866d813b8d1";
+  final testNoteModel = NoteModel(
+      id: "110ec58a-a0f2-4ac4-8393-c866d813b8d1",
+      title: "title",
+      content: "content",
+      createdAt: DateTime.parse("2023-02-22T19:29:39.242"),
+      lastEdited: DateTime.parse("2023-02-22T19:29:39.242"));
+  final testNoteModelList = [testNoteModel];
+  final Note testNote = testNoteModel;
+
+  test('Should check if device is offline', () {
+    when(mockNetworkInfo.isConnected)
+        .thenAnswer((realInvocation) async => true);
+
+    repository.getNote(testId);
+
+    verify(mockNetworkInfo.isConnected);
+  });
+
+  group('If device is online', () {
+    setUp(() => when(mockNetworkInfo.isConnected)
+        .thenAnswer((realInvocation) async => true));
+
+    test('Should return remote data', () async {
+      when(mockNoteRemoteDataSource.getNotes()).thenAnswer((realInvocation) async => testNoteModelList);
+
+      final actual = await repository.getNotes();
+
+      verify(mockNoteRemoteDataSource.getNotes());
+      expect(actual, equals(Right(testNoteModelList)));
+    });
   });
 }
