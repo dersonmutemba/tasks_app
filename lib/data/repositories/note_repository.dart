630f3e0f48aca595f58cce7adf6ec709e8c6f838
dersonmutemba <dersonmutemba@gmail.com>
2023-02-23 22:17:42 +0200

    added mocks and constructor for `NoteRepository`

--- lib/data/repositories/note_repository.dart
+++ lib/data/repositories/note_repository.dart
@@ -1,10 +1,22 @@
 import 'package:dartz/dartz.dart';
 
 import '../../core/error/failure.dart';
+import '../../core/platform/network_info.dart';
 import '../../domain/contracts/note_contract.dart';
 import '../../domain/entities/note.dart';
+import '../datasources/note_local_data_source.dart';
+import '../datasources/note_remote_data_source.dart';
 
 class NoteRepository implements NoteContract {
+  final NoteRemoteDataSource remoteDataSource;
+  final NoteLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
+
+  NoteRepository(
+      {required this.remoteDataSource,
+      required this.localDataSource,
+      required this.networkInfo});
+
   @override
   Future<Either<Failure, Note>> getNote(String id) {
     // TODO: implement getNote
@@ -16,5 +28,4 @@ class NoteRepository implements NoteContract {
     // TODO: implement getNotes
     throw UnimplementedError();
   }
-
 }
--- test/data/repositories/note_repository_test.dart
+++ test/data/repositories/note_repository_test.dart
@@ -4,11 +4,26 @@ import 'package:mockito/mockito.dart';
 import 'package:tasks_app/core/platform/network_info.dart';
 import 'package:tasks_app/data/datasources/note_local_data_source.dart';
 import 'package:tasks_app/data/datasources/note_remote_data_source.dart';
+import 'package:tasks_app/data/repositories/note_repository.dart';
+
+import 'note_repository_test.mocks.dart';
 
 @GenerateNiceMocks([MockSpec<NetworkInfo>()])
 @GenerateNiceMocks([MockSpec<NoteLocalDataSource>()])
 @GenerateNiceMocks([MockSpec<NoteRemoteDataSource>()])
-
 void main() {
+  late NoteRepository repository;
+  late MockNoteRemoteDataSource mockNoteRemoteDataSource;
+  late MockNoteLocalDataSource mockNoteLocalDataSource;
+  late MockNetworkInfo mockNetworkInfo;
 
+  setUp(() {
+    mockNoteRemoteDataSource = MockNoteRemoteDataSource();
+    mockNoteLocalDataSource = MockNoteLocalDataSource();
+    mockNetworkInfo = MockNetworkInfo();
+    repository = NoteRepository(
+        remoteDataSource: mockNoteRemoteDataSource,
+        localDataSource: mockNoteLocalDataSource,
+        networkInfo: mockNetworkInfo);
+  });
 }
