b4c5bd4e4b981b05c8e584559aa7c6b8a363ee64
dersonmutemba <dersonmutemba@gmail.com>
2023-02-24 19:40:10 +0200

    write tests for `NetworkInfo`

--- test/core/network/network_info_test.dart
+++ test/core/network/network_info_test.dart
@@ -1,26 +1,28 @@
-// @dart=2.9
-
-import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:tasks_app/core/network/data_connection_checker.dart';
 import 'package:tasks_app/core/network/network_info.dart';
 
-class MockDataConnectionChecker extends Mock implements DataConnectionChecker{}
+import 'network_info_test.mocks.dart';
 
+@GenerateNiceMocks([MockSpec<DataConnectionChecker>()])
 void main() {
-  NetworkInfoImplementation networkInfo;
-  MockDataConnectionChecker mockDataConnectionChecker;
+  late NetworkInfoImplementation networkInfo;
+  late MockDataConnectionChecker mockDataConnectionChecker;
 
   setUp(() {
     mockDataConnectionChecker = MockDataConnectionChecker();
     networkInfo = NetworkInfoImplementation(mockDataConnectionChecker);
   });
-  
+
   group('isConnected property', () {
-    test('Should forward the call to DataConnectionChecker.hasConnection', () async {
+    test('Should forward the call to DataConnectionChecker.hasConnection',
+        () async {
       final matcher = Future.value(true);
 
-      when(mockDataConnectionChecker.hasConnection).thenAnswer((realInvocation) => matcher);
+      when(mockDataConnectionChecker.hasConnection)
+          .thenAnswer((realInvocation) => matcher);
 
       final actual = networkInfo.isConnected;
 
