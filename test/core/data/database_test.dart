c07d9e53ee16c14fb8ae69d9918ce17e035c17a2
dersonmutemba <dersonmutemba@gmail.com>
2023-02-25 21:17:42 +0200

    completed CRUD

--- test/core/data/database_test.dart
+++ test/core/data/database_test.dart
@@ -20,7 +20,29 @@ void main() {
       content: 'content',
       createdAt: DateTime.parse('2023-02-22T19:29:39.242'),
       lastEdited: DateTime.parse('2023-02-22T19:29:39.242'));
-  final List<Map<String, dynamic>> testNoteList = List.generate(10, (index) => testNote.toJson());
+  final List<Map<String, dynamic>> testNoteList = [
+    NoteModel(
+            id: '110ec58a-a0f2-4ac4-8393-c866d813b8d1',
+            title: 'title',
+            content: 'content',
+            createdAt: DateTime.parse('2023-02-22T19:29:39.242'),
+            lastEdited: DateTime.parse('2023-02-22T19:29:39.242'))
+        .toJson(),
+    NoteModel(
+            id: '310ec58c-a0f2-4ac4-8393-c866d813b8d1',
+            title: 'title',
+            content: 'content',
+            createdAt: DateTime.parse('2023-02-22T19:29:39.242'),
+            lastEdited: DateTime.parse('2023-02-22T19:29:39.242'))
+        .toJson(),
+    NoteModel(
+            id: '160ec58a-a0f2-4ac4-8393-c866d813b8d1',
+            title: 'title',
+            content: 'content',
+            createdAt: DateTime.parse('2023-02-22T19:29:39.242'),
+            lastEdited: DateTime.parse('2023-02-22T19:29:39.242'))
+        .toJson(),
+  ];
   setUpAll(() async {
     sqfliteFfiInit();
     database = await databaseFactoryFfi.openDatabase(inMemoryDatabasePath);
@@ -33,11 +55,14 @@ void main() {
         ${keys.elementAt(3)} DATETIME NOT NULL,
         ${keys.elementAt(4)} DATETIME NOT NULL
       );''');
-      mockLocalDatabase = MockLocalDatabase();
-      mockLocalDatabase.db = database;
-      when(mockLocalDatabase.update(any, any, any, any)).thenAnswer((realInvocation) async => 1);
-      when(mockLocalDatabase.delete(any, any, any)).thenAnswer((realInvocation) async => 1);
-      when(mockLocalDatabase.getObjects(any, any, any, any)).thenAnswer((realInvocation) async => testNoteList);
+    mockLocalDatabase = MockLocalDatabase();
+    mockLocalDatabase.db = database;
+    when(mockLocalDatabase.update(any, any, any, any))
+        .thenAnswer((realInvocation) async => 1);
+    when(mockLocalDatabase.delete(any, any, any))
+        .thenAnswer((realInvocation) async => 1);
+    when(mockLocalDatabase.getObjects(any, any, any, any))
+        .thenAnswer((realInvocation) async => testNoteList);
   });
 
   group('Test Sqflite Database', () {
@@ -45,10 +70,44 @@ void main() {
       expect(await database.getVersion(), dbVersion);
     });
 
-    test('Add Note to database', () async {
+    test('Insert Note to database', () async {
       await database.insert(noteTable, testNote.toJson());
       var actual = (await database.query(noteTable)).length;
       expect(actual, 1);
+    });
+
+    test('Update Note in the database', () async {
+      const String matcher = 'another content';
+      final NoteModel noteModel = NoteModel(
+          id: testNote.id,
+          title: 'title',
+          content: matcher,
+          createdAt: testNote.createdAt,
+          lastEdited: testNote.lastEdited);
+      var keys = testNote.toJson().keys;
+      await database.update(noteTable, noteModel.toJson(),
+          where: '${keys.first} = ?', whereArgs: [noteModel.id]);
+      var actual = (await database.query(noteTable)).first['content'];
+      expect(actual, matcher);
+    });
+
+    test('Delete Note in the database', () async {
+      var keys = testNote.toJson().keys;
+      await database.delete(noteTable,
+          where: '${keys.first} = ?', whereArgs: [testNote.id]);
+      var actual = (await database.query(noteTable)).length;
+      expect(actual, 0);
+    });
+
+    test('Insert multiple Notes in the database', () async {
+      Batch batch = database.batch();
+      for (var map in testNoteList) {
+        batch.insert(noteTable, map,
+            conflictAlgorithm: ConflictAlgorithm.replace);
+      }
+      await batch.commit();
+      var actual = (await database.query(noteTable)).length;
+      expect(actual, testNoteList.length);
     });
   });
 }
