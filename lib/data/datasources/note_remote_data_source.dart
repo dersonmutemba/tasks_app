2600e5766c6f0e06661021e97fc00742ac5eb95b
dersonmutemba <dersonmutemba@gmail.com>
2023-03-01 00:46:58 +0200

    prepared remote datasource for future development

--- lib/data/datasources/note_remote_data_source.dart
+++ lib/data/datasources/note_remote_data_source.dart
@@ -1,5 +1,18 @@
+import 'package:http/http.dart' as http;
+
 import '../models/note_model.dart';
 
 abstract class NoteRemoteDataSource {
   Future<List<NoteModel>> getNotes();
+}
+
+class NoteRemoteDataSourceImplementation implements NoteRemoteDataSource {
+  final http.Client client;
+  NoteRemoteDataSourceImplementation({required this.client});
+
+  @override
+  Future<List<NoteModel>> getNotes() {
+    // TODO: implement getNotes
+    throw UnimplementedError();
+  }
 }
--- test/data/datasources/note_remote_data_source_test.dart
+++ test/data/datasources/note_remote_data_source_test.dart
@@ -1,17 +1,7 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:mockito/annotations.dart';
 
-import 'note_remote_data_source_test.mocks.dart';
-
 @GenerateNiceMocks([MockSpec<http.Client>()])
 void main() {
-  late NoteRemoteDataSourceImplementation noteRemoteDataSource;
-  late MockClient mockClient;
-
-  setUp(() {
-    mockClient = MockClient();
-    noteRemoteDataSource =
-        NoteRemoteDataSourceImplementation(client: mockClient);
-  });
+  
 }
