33f406242eddb348ec4e7385497a2b48e8c6e1b1
dersonmutemba <dersonmutemba@gmail.com>
2023-03-02 23:31:44 +0200

    added bloc event and state for `Note`

---
+++ lib/presentation/bloc/note_event.dart
@@ -0,0 +1,16 @@
+import 'package:meta/meta.dart';
+
+@immutable
+abstract class NoteEvent {
+  final List props;
+  const NoteEvent([this.props = const <dynamic>[]]);
+
+  @override
+  bool operator == (Object other) =>
+    other is NoteEvent &&
+    other.runtimeType == runtimeType &&
+    other.props == props;
+  
+  @override
+  int get hashCode => props.hashCode;
+}
