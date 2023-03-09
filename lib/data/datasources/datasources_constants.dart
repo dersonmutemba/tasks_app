1c863fd55b32ceee501f14fd63190e98dab616e5
dersonmutemba <dersonmutemba@gmail.com>
2023-03-09 11:27:14 +0200

    fixed stackOverflow exception when testing

--- lib/data/datasources/datasources_constants.dart
+++ lib/data/datasources/datasources_constants.dart
@@ -1,12 +1,17 @@
+Map<String, dynamic> _datasourcesConstants = {
+  'noteTable': 'note',
+  'noteColumns': ['id', 'title', 'content', 'createdAt', 'lastEdited'],
+};
+
 Map<String, dynamic> datasourcesConstants = {
   'noteTable': 'note',
   'noteColumns': ['id', 'title', 'content', 'createdAt', 'lastEdited'],
   'noteTableQuery': '''
-      CREATE TABLE ${datasourcesConstants['noteTable']}(
-        ${datasourcesConstants['noteTable'][0]} TEXT PRIMARY KEY,
-        ${datasourcesConstants['noteTable'][1]} TEXT NOT NULL,
-        ${datasourcesConstants['noteTable'][2]} TEXT NOT NULL,
-        ${datasourcesConstants['noteTable'][3]} DATETIME NOT NULL,
-        ${datasourcesConstants['noteTable'][4]} DATETIME NOT NULL
+      CREATE TABLE if not exists ${_datasourcesConstants['noteTable']}(
+        ${_datasourcesConstants['noteColumns'][0]} TEXT PRIMARY KEY,
+        ${_datasourcesConstants['noteColumns'][1]} TEXT NOT NULL,
+        ${_datasourcesConstants['noteColumns'][2]} TEXT NOT NULL,
+        ${_datasourcesConstants['noteColumns'][3]} DATETIME NOT NULL,
+        ${_datasourcesConstants['noteColumns'][4]} DATETIME NOT NULL
       );'''
 };
