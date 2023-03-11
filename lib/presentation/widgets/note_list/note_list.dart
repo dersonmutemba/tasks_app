342d5a34f7d3c1825e74211eb9146ecf77a3db45
dersonmutemba <dersonmutemba@gmail.com>
2023-03-11 22:33:46 +0200

    changed location of note list file

--- lib/presentation/widgets/note_list/note_list.dart
+++ lib/presentation/widgets/note_list/note_list.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../injection_container.dart';
-import '../bloc/bloc.dart';
+import '../../../injection_container.dart';
+import '../../bloc/bloc.dart';
 
 class NoteList extends StatelessWidget {
   const NoteList({Key? key}) : super(key: key);
