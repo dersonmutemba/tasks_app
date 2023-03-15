bac09938b69e1b7738be384860217b186e790acf
dersonmutemba <dersonmutemba@gmail.com>
2023-03-15 23:59:14 +0200

    changed bloc to include fab

--- lib/presentation/pages/home/home.dart
+++ lib/presentation/pages/home/home.dart
@@ -11,63 +11,62 @@ class Home extends StatelessWidget {
     return BlocProvider(
       create: (_) => HomeBloc(),
       child: BlocBuilder<HomeBloc, HomeState>(
-            builder: ((context, state) {
-              return Scaffold(
-        body: SafeArea(
-          child: TextButton(
-                onPressed: () {
-                  if (state is SelectedTasksHome) {
-                    context.read<HomeBloc>().add(NotesHomeSelected());
-                  } else if (state is SelectedNotesHome) {
-                    context.read<HomeBloc>().add(TasksHomeSelected());
-                  }
-                },
-                style: TextButton.styleFrom(
-                  foregroundColor: Theme.of(context).textTheme.bodyLarge!.color,
+        builder: ((context, state) {
+          return Scaffold(
+            body: SafeArea(
+                child: TextButton(
+              onPressed: () {
+                if (state is SelectedTasksHome) {
+                  context.read<HomeBloc>().add(NotesHomeSelected());
+                } else if (state is SelectedNotesHome) {
+                  context.read<HomeBloc>().add(TasksHomeSelected());
+                }
+              },
+              style: TextButton.styleFrom(
+                foregroundColor: Theme.of(context).textTheme.bodyLarge!.color,
+              ),
+              child: SizedBox(
+                height: 50,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      state.title,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Column(
+                      children: const [
+                        Icon(Icons.keyboard_arrow_up_rounded),
+                        Icon(Icons.keyboard_arrow_down_rounded)
+                      ],
+                    ),
+                  ],
                 ),
-                child: SizedBox(
-                  height: 50,
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        state.title,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 5,
-                      ),
-                      Column(
-                        children: const [
-                          Icon(Icons.keyboard_arrow_up_rounded),
-                          Icon(Icons.keyboard_arrow_down_rounded)
-                        ],
-                      ),
-                    ],
-                  ),
+              ),
+            )),
+            floatingActionButton: ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                shape: MaterialStateProperty.all(const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15)))),
+              ),
+              child: Container(
+                height: 40,
+                width: 40,
+                padding: EdgeInsets.zero,
+                child: const Icon(
+                  Icons.add_rounded,
+                  size: 30,
                 ),
-              )
+              ),
             ),
-          );,
-        );};,
-        floatingActionButton: ElevatedButton(
-          onPressed: () {},
-          style: ButtonStyle(
-            shape: MaterialStateProperty.all(const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(15)))),
-          ),
-          child: Container(
-            height: 40,
-            width: 40,
-            padding: EdgeInsets.zero,
-            child: const Icon(
-              Icons.add_rounded,
-              size: 30,
-            ),
-          ),
-        ),
+          );
+        }),
       ),
     );
   }
