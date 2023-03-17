026fae4875de4da39462805a43baaadbbefb221b
dersonmutemba <dersonmutemba@gmail.com>
2023-03-17 11:06:55 +0200

    added style to iconbutton

--- lib/presentation/pages/note/note_page.dart
+++ lib/presentation/pages/note/note_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-import '../../widgets/my_icon.dart';
+import '../../widgets/my_icon_button.dart';
 
 class NotePage extends StatelessWidget {
   const NotePage({Key? key}) : super(key: key);
@@ -17,14 +17,18 @@ class NotePage extends StatelessWidget {
                 const SizedBox(
                   width: 10,
                 ),
-                const MyIcon(iconData: Icons.cancel_outlined),
+                MyIconButton(
+                  iconData: Icons.arrow_back_ios,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
                 const Spacer(),
-                OutlinedButton(
-                  onPressed: () {},
-                  style: OutlinedButton.styleFrom(
-                    
-                  ),
-                  child: const MyIcon(iconData: Icons.check),
+                MyIconButton(
+                  iconData: Icons.more_vert,
+                  onPressed: () {
+                    // TODO: Add a popup menu and some options
+                  },
                 ),
                 const SizedBox(
                   width: 10,
