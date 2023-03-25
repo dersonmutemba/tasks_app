a582f80c818d8afed1f62d0888147dd427a33621
dersonmutemba <dersonmutemba@gmail.com>
2023-03-25 12:15:48 +0200

    remove unnecessary import

--- test/core/data/database_test.dart
+++ test/core/data/database_test.dart
@@ -3,7 +3,6 @@ import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'package:tasks_app/core/data/constants.dart';
 import 'package:tasks_app/core/data/database.dart';
 import 'package:tasks_app/data/models/note_model.dart';
 
