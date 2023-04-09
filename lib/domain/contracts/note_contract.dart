80e29c0c68b9687a86dbd5ca5a96528b74d55490
dersonmutemba <dersonmutemba@gmail.com>
2023-04-09 05:56:40 +0200

    add update note logic

--- lib/domain/contracts/note_contract.dart
+++ lib/domain/contracts/note_contract.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
-import 'package:tasks_app/core/error/failure.dart';
 
+import '../../core/error/failure.dart';
 import '../../core/success/success.dart';
 import '../entities/note.dart';
 
@@ -8,4 +8,5 @@ abstract class NoteContract {
   Future<Either<Failure, Success>> insertNote(Note note);
   Future<Either<Failure, Note>> getNote(String id);
   Future<Either<Failure, List<Note>>> getNotes();
+  Future<Either<Failure, Success>> updateNote(Note note);
 }
---
+++ lib/domain/usecases/update_note.dart
@@ -0,0 +1,27 @@
+import 'package:dartz/dartz.dart';
+
+import '../../core/error/failure.dart';
+import '../../core/success/success.dart';
+import '../../core/usecases/usecase.dart';
+import '../contracts/note_contract.dart';
+import '../entities/note.dart';
+
+class UpdateNote extends UseCase<Success, Params> {
+  final NoteContract contract;
+  UpdateNote(this.contract);
+
+  @override
+  Future<Either<Failure, Success>> call(Params params) async => contract.updateNote(params.note);
+}
+
+class Params {
+  final Note note;
+  Params({required this.note});
+
+  @override
+  bool operator ==(Object other) =>
+      other is Params && other.runtimeType == runtimeType && other.note == note;
+
+  @override
+  int get hashCode => note.hashCode;
+}
