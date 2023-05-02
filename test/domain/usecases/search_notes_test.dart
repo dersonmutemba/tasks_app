18d7967d171eaa1c2ffdebc836a0d96ff88a7bfc
dersonmutemba <dersonmutemba@gmail.com>
2023-05-02 20:11:38 +0200

    added tests for search notes

---
+++ test/domain/usecases/search_notes_test.dart
@@ -0,0 +1,37 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:tasks_app/domain/entities/note.dart';
+
+import 'note_contract_mock.mocks.dart';
+
+void main() {
+  late MockNoteContract mockNoteContract;
+  late SearchNotes usecase;
+
+  setUp(() {
+    mockNoteContract = MockNoteContract();
+    usecase = SearchNotes(mockNoteContract);
+  });
+
+  const testSearchQuery = 'title';
+  final testNotes = [
+    Note(
+        id: "id",
+        title: "title",
+        content: "content",
+        createdAt: DateTime.now(),
+        lastEdited: DateTime.now())
+  ];
+
+  test('Should search Notes in the repository', () async {
+    when(mockNoteContract.searchNote(any))
+        .thenAnswer((value) async => Right(testNotes));
+
+    final matcher = await usecase(Params(query: testSearchQuery));
+
+    expect(Right(testNotes), matcher);
+    verify(mockNoteContract.searchNote(testSearchQuery));
+    verifyNoMoreInteractions(mockNoteContract);
+  });
+}
