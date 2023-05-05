46f602f70226e3f943fe23f19815f222f472aef4
Derson Mutemba <55860970+dersonmutemba@users.noreply.github.com>
2023-05-05 23:30:27 +0200

    added action for search event

--- lib/presentation/widgets/notes_container/note_list/bloc/note_list_bloc.dart
+++ lib/presentation/widgets/notes_container/note_list/bloc/note_list_bloc.dart
@@ -3,11 +3,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../../core/error/failure.dart';
 import '../../../../../core/usecases/usecase.dart';
 import '../../../../../domain/usecases/get_notes.dart';
+import '../../../../../domain/usecases/search_notes.dart';
 import 'bloc.dart';
 
 class NoteListBloc extends Bloc<NoteListEvent, NoteListState> {
   final GetNotes getNotes;
-  NoteListBloc({required this.getNotes}) : super(Loading()) {
+  final SearchNotes searchNotes;
+  NoteListBloc({required this.getNotes, required this.searchNotes})
+      : super(Loading()) {
     on<Load>(
       (event, emit) async {
         emit(Loading());
@@ -16,6 +19,25 @@ class NoteListBloc extends Bloc<NoteListEvent, NoteListState> {
           if (l is ServerFailure) {
             emit(Error(message: 'Server Error'));
           } else if (l is CacheFailure) {
+            emit(Error(message: 'Local Database Error'));
+          } else {
+            emit(Error(message: 'Unknown Error'));
+          }
+        }, (r) {
+          if (r.isEmpty) {
+            emit(Empty());
+          } else {
+            emit(Loaded(r));
+          }
+        });
+      },
+    );
+
+    on<Search>(
+      (event, emit) async {
+        var result = await searchNotes(Params(query: event.query));
+        result.fold((l) {
+          if (l is CacheFailure) {
             emit(Error(message: 'Local Database Error'));
           } else {
             emit(Error(message: 'Unknown Error'));
