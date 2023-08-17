e3e10ebd854b5f5bdcb683583bd20a4da4b5e9e3
dersonmutemba <dersonmutemba@gmail.com>
2023-08-18 01:32:26 +0200

    add method to return Status by String

--- lib/domain/entities/enumuration/status.dart
+++ lib/domain/entities/enumuration/status.dart
@@ -3,5 +3,16 @@ enum Status {
   aborted,
   completed,
   paused,
-  notStarted
+  notStarted,
+  unknown,
+}
+
+Status? statusFromString(String text) {
+  text = text.split('.').last;
+  for(Status status in Status.values) {
+    if(status.name.toLowerCase() == text.toLowerCase()) {
+      return status;
+    }
+  }
+  return null;
 }
