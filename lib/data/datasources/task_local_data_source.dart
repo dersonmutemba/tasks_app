a9438b931340181eeac2c51825817f0a4d4750f9
dersonmutemba <dersonmutemba@gmail.com>
2023-08-30 20:33:44 +0200

    add logic for tasks local datasource

--- lib/data/datasources/task_local_data_source.dart
+++ lib/data/datasources/task_local_data_source.dart
@@ -1,4 +1,7 @@
+import '../../core/data/database.dart';
+import '../../core/error/exception.dart';
 import '../models/task_model.dart';
+import 'datasources_constants.dart';
 
 abstract class TaskLocalDataSource {
   Future<TaskModel> getTask(String id);
@@ -17,46 +20,74 @@ abstract class TaskLocalDataSource {
 }
 
 class TaskLocalDataSourceImplementation implements TaskLocalDataSource {
+  LocalDatabase localDatabase;
+  final String table = datasourcesConstants['taskTable'];
+  final List<String> columns = datasourcesConstants['taskColumns'];
+  TaskLocalDataSourceImplementation(this.localDatabase);
+
   @override
-  Future<void> cacheTasks(List<TaskModel> tasks) {
-    // TODO: implement cacheTasks
-    throw UnimplementedError();
+  Future<void> cacheTasks(List<TaskModel> tasks) async {
+    List<Map<String, dynamic>> valuesList = [];
+    for (var task in tasks) {
+      valuesList.add(task.toJson());
+    }
+    await localDatabase.insert(table, valuesList);
   }
 
   @override
-  Future<void> deleteTask(String id) {
-    // TODO: implement deleteTask
-    throw UnimplementedError();
+  Future<void> deleteTask(String id) async {
+    await localDatabase.delete(table, [columns.first], [id]);
   }
 
   @override
-  Future<TaskModel> getTask(String id) {
-    // TODO: implement getTask
-    throw UnimplementedError();
+  Future<TaskModel> getTask(String id) async {
+    var result = await localDatabase.getObjects(
+        table, [columns.first], [id], columns.sublist(1));
+    if (result != null) {
+      return TaskModel.fromJson(result.first);
+    }
+    throw CacheException();
   }
 
   @override
-  Future<List<TaskModel>> getTasks() {
-    // TODO: implement getTasks
-    throw UnimplementedError();
+  Future<List<TaskModel>> getTasks() async {
+    var results = await localDatabase.getAllObjects(table);
+    if (results != null) {
+      List<TaskModel> tasks = [];
+      for (var result in results) {
+        tasks.add(TaskModel.fromJson(result));
+      }
+      return tasks;
+    }
+    throw CacheException();
   }
 
   @override
-  Future<String> insertTask(TaskModel task) {
-    // TODO: implement insertTask
-    throw UnimplementedError();
+  Future<String> insertTask(TaskModel task) async {
+    localDatabase.insert(table, [task.toJson()]);
+    return task.id;
   }
 
   @override
-  Future<List<TaskModel>> searchTasks(String query) {
-    // TODO: implement searchTasks
-    throw UnimplementedError();
+  Future<List<TaskModel>> searchTasks(String query) async {
+    var results = await localDatabase.searchObjects(
+        table,
+        columns.sublist(1, 3)..add(columns.last),
+        query,
+        columns.sublist(3, columns.length - 1)..add(columns[0]));
+    if (results != null) {
+      List<TaskModel> tasks = [];
+      for (var result in results) {
+        tasks.add(TaskModel.fromJson(result));
+      }
+      return tasks;
+    }
+    return [];
   }
 
   @override
-  Future<void> updateTask(TaskModel task) {
-    // TODO: implement updateTask
-    throw UnimplementedError();
+  Future<void> updateTask(TaskModel task) async {
+    await localDatabase
+        .update(table, task.toJson(), [columns.first], [task.id]);
   }
-
 }
