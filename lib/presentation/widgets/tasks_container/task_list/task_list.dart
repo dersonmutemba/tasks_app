73883a70fe64b9ced75bf0c1428886c3d41a5235
dersonmutemba <dersonmutemba@gmail.com>
2023-09-22 00:59:43 +0200

    add task list components

--- lib/presentation/widgets/tasks_container/task_list/task_list.dart
+++ lib/presentation/widgets/tasks_container/task_list/task_list.dart
@@ -1,11 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../injection_container.dart';
+import '../../my_search_bar.dart';
+import 'bloc/bloc.dart';
 
 class TaskList extends StatelessWidget {
   const TaskList({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    TextEditingController searchController = TextEditingController();
+    var taskListBloc = serviceLocator<TaskListBloc>();
+
+    Widget searchContainer = MySearchBar(
+      controller: searchController,
+      onChanged: (value) {
+        taskListBloc.add(Search(value));
+      },
+    );
+
+    return BlocProvider(
+      create: (arg) => taskListBloc..add(Load()),
+      child: BlocBuilder<TaskListBloc, TaskListState>(
+        builder: (context, state) {
+          if (state is Empty) {
+            return const Center(
+              child: Text('No tasks saved yet. Click on "+" to add one'),
+            );
+          } else if (state is NotFound) {
+            return Column(
+              children: [
+                searchContainer,
+                const Expanded(
+                  child: Center(
+                    child: Text('No task were found that matched the query'),
+                  ),
+                )
+              ],
+            );
+          } else if (state is Error) {
+            return Center(child: Text(state.message),);
+          } else if (state is Loading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (state is Loaded) {
+            return Column(
+              children: [
+                searchContainer,
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: state.tasks.length,
+                    itemBuilder: (context, index) {
+                      
+                    },
+                  ),
+                )
+              ],
+            );
+          }
+          return Container();
+        },
+      ),
+    );
   }
 }
