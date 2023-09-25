c0628594283fc88aa64dccede6b179543e94432c
dersonmutemba <dersonmutemba@gmail.com>
2023-09-25 16:21:38 +0200

    wrapped creation and edition widget inside Container

--- lib/presentation/pages/task_page/task_page.dart
+++ lib/presentation/pages/task_page/task_page.dart
@@ -40,120 +40,113 @@ class TaskPage extends StatelessWidget {
                     taskNameController.text = state.task.name;
                     taskDescriptionController.text = state.task.description!;
                   }
-                  return Column(
-                    children: [
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Row(
-                        children: [
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          MyIconButton(
-                            iconData: Icons.arrow_back_ios,
-                            tooltip: 'Go back',
-                            onPressed: () async {
-                              if (context.mounted) {
-                                Navigator.pop(context);
-                              }
-                            },
-                          ),
-                          const Spacer(),
-                          MyIconButton(
-                            iconData: Icons.more_vert,
-                            tooltip: 'More options',
-                            onPressed: () {},
-                          ),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                        ],
-                      ),
-                      Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 15),
-                        child: Row(
+                  return Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                    child: Column(
+                      children: [
+                        Row(
                           children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(25),
-                                color: Colors.blue,
-                              ),
-                              height: 40,
-                              width: 40,
-                              child: const Icon(
-                                Icons.photo_outlined,
-                                color: Colors.white,
-                              ),
+                            MyIconButton(
+                              iconData: Icons.arrow_back_ios,
+                              tooltip: 'Go back',
+                              onPressed: () async {
+                                if (context.mounted) {
+                                  Navigator.pop(context);
+                                }
+                              },
                             ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Expanded(
-                              child: TextField(
-                                maxLines: 1,
-                                keyboardType: TextInputType.text,
-                                textCapitalization:
-                                    TextCapitalization.sentences,
-                                controller: taskNameController,
-                                decoration: const InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: 'Name...',
-                                  contentPadding:
-                                      EdgeInsets.symmetric(vertical: 10),
-                                ),
-                                style:
-                                    Theme.of(context).textTheme.headlineMedium,
-                                focusNode: taskNameFocusNode,
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(25),
-                        child: Row(
-                          children: [
-                            Expanded(
-                              child: TextButton(
-                                style: TextButton.styleFrom(
-                                  shape: const RoundedRectangleBorder(
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(25)),
-                                  ),
-                                  backgroundColor:
-                                      const Color.fromARGB(150, 225, 225, 225),
-                                  foregroundColor: Theme.of(context)
-                                      .textTheme
-                                      .bodyMedium!
-                                      .color,
-                                ),
-                                child: const Text('Due Tomorrow'),
-                                onPressed: () {},
-                              ),
-                            ),
-                            const SizedBox(width: 20,),
-                            Expanded(
-                              child: TextButton(
-                                style: TextButton.styleFrom(
-                                  shape: const RoundedRectangleBorder(
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(25)),
-                                  ),
-                                  backgroundColor:
-                                      const Color.fromARGB(150, 225, 225, 225),
-                                  foregroundColor: Theme.of(context)
-                                      .textTheme
-                                      .bodyMedium!
-                                      .color,
-                                ),
-                                child: const Text('Cancelled'),
-                                onPressed: () {},
-                              ),
+                            const Spacer(),
+                            MyIconButton(
+                              iconData: Icons.more_vert,
+                              tooltip: 'More options',
+                              onPressed: () {},
                             ),
                           ],
                         ),
-                      ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Row(
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(25),
+                                  color: Colors.blue,
+                                ),
+                                height: 40,
+                                width: 40,
+                                child: const Icon(
+                                  Icons.photo_outlined,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Expanded(
+                                child: TextField(
+                                  maxLines: 1,
+                                  keyboardType: TextInputType.text,
+                                  textCapitalization:
+                                      TextCapitalization.sentences,
+                                  controller: taskNameController,
+                                  decoration: const InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: 'Name...',
+                                    contentPadding:
+                                        EdgeInsets.symmetric(vertical: 10),
+                                  ),
+                                  style:
+                                      Theme.of(context).textTheme.headlineMedium,
+                                  focusNode: taskNameFocusNode,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: TextButton(
+                                  style: TextButton.styleFrom(
+                                    shape: const RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(25)),
+                                    ),
+                                    backgroundColor:
+                                        const Color.fromARGB(150, 225, 225, 225),
+                                    foregroundColor: Theme.of(context)
+                                        .textTheme
+                                        .bodyMedium!
+                                        .color,
+                                  ),
+                                  child: const Text('Due Tomorrow'),
+                                  onPressed: () {},
+                                ),
+                              ),
+                              const SizedBox(width: 20,),
+                              Expanded(
+                                child: TextButton(
+                                  style: TextButton.styleFrom(
+                                    shape: const RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(25)),
+                                    ),
+                                    backgroundColor:
+                                        const Color.fromARGB(150, 225, 225, 225),
+                                    foregroundColor: Theme.of(context)
+                                        .textTheme
+                                        .bodyMedium!
+                                        .color,
+                                  ),
+                                  child: const Text('Cancelled'),
+                                  onPressed: () {},
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                         Expanded(
                           child: TextField(
                             expands: true,
@@ -172,7 +165,8 @@ class TaskPage extends StatelessWidget {
                             style: Theme.of(context).textTheme.bodyMedium,
                           ),
                         ),
-                    ],
+                      ],
+                    ),
                   );
                 } else if (state is Loading || state is Saving) {
                   return const Center(
