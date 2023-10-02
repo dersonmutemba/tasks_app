1acf25556abbbf469436edd0edb179206864ac98
dersonmutemba <dersonmutemba@gmail.com>
2023-10-02 12:03:28 +0200

    add friendly display of due date

--- lib/presentation/pages/task_page/task_page.dart
+++ lib/presentation/pages/task_page/task_page.dart
@@ -189,6 +189,11 @@ class _TaskPageState extends State<TaskPage> {
   }
 
   String _resolveDateTime(DateTime dateTime) {
+    DateTime now = DateTime.now();
+    if (now.month == dateTime.month && now.year == dateTime.year) {
+      if (now.day == dateTime.day) return 'Today';
+      if (now.day == dateTime.day - 1) return 'Tomorrow';
+    }
     return '${dateTime.day}/${dateTime.month}/${dateTime.year}';
   }
 }
