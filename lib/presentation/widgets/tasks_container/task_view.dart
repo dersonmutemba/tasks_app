82159ec2ec4a549e0364564e3d8df7371785e5b5
dersonmutemba <dersonmutemba@gmail.com>
2023-10-04 11:21:41 +0200

    create TaskView Widget

---
+++ lib/presentation/widgets/tasks_container/task_view.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../../../domain/entities/task.dart';
+
+class TaskView extends StatelessWidget {
+  final Task task;
+  final Function openTask;
+  const TaskView({Key? key, required this.task, required this.openTask})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      title: Text(
+        task.name,
+        maxLines: 1,
+        style: Theme.of(context).textTheme.titleMedium,
+      ),
+      subtitle: Container(
+        padding: const EdgeInsets.only(top: 8, bottom: 4),
+        child: Text(
+          task.description!,
+          style: Theme.of(context).textTheme.labelMedium,
+        ),
+      ),
+      enableFeedback: true,
+      onTap: () async {
+        await openTask();
+      },
+    );
+  }
+}
